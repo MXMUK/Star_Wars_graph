ef610a545dfb477c4d409cb469ce31e56a7b5272
mxmk <romamaksymuk@gmail.com>
2024-05-02 17:54:35 +0200

    fixed errors for deploy on vercel

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,38 +1,8 @@
-import Image from "next/image";
-import { Card } from "@repo/ui/card";
-
-function Gradient({
-  conic,
-  className,
-  small,
-}: {
-  small?: boolean;
-  conic?: boolean;
-  className?: string;
-}): JSX.Element {
-  return (
-    <span
-      className={`absolute mix-blend-normal will-change-[filter] rounded-[100%] ${
-        small ? "blur-[32px]" : "blur-[75px]"
-      } ${conic ? "bg-glow-conic" : ""} ${className}`}
-    />
-  );
-}
 
 export default function Page(): JSX.Element {
   return (
-    <main className="flex flex-col items-center justify-between min-h-screen p-24">
-
-      <div className="relative flex place-items-center ">
-        <div className="font-sans w-auto pb-16 pt-[48px] md:pb-24 lg:pb-32 md:pt-16 lg:pt-20 flex justify-between gap-8 items-center flex-col relative z-0">
-          <div className="z-50 flex items-center justify-center w-full">
-          </div>
-          <Gradient
-            className="top-[-500px] opacity-[0.15] w-[1000px] h-[1000px]"
-            conic
-          />
-        </div>
-      </div>
+    <main className="">
+      dfsdf   
     </main>
   );
 }
