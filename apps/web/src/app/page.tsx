d78a6cf81e12b5dd011741cbc5179d1b52ff0087
mxmk <romamaksymuk@gmail.com>
2024-05-13 13:59:25 +0200

    list of heros & pagination & animation & styling & custom node & fetch all data & all nodes are implemented

--- apps/web/src/app/page.tsx
+++ apps/web/src/app/page.tsx
@@ -1,38 +1,40 @@
+/* eslint-disable no-unused-vars -- delete me */
+/* eslint-disable @typescript-eslint/no-unused-vars -- delete me */
 'use client';
 
-// import { useCallback, useEffect, useState } from 'react';
-// import { useSearchParams } from 'next/navigation';
-// import type { Person } from '../types/person';
-// import { getAllHeroes } from '../api/people';
+import { useCallback, useEffect, useState } from 'react';
+import { useSearchParams } from 'next/navigation';
+import type { Person } from '../types/person';
+import { getAllHeroes } from '../api/people';
 // import { PersonCard } from '../components/person-card';
 // import { PersonCardSkeleton } from '../components/skeletons/person-card-skeleton';
 // import { Pagination } from '../components/pagination';
 
 const Page = (): JSX.Element => {
-  // const [people, setPeople] = useState<Person[]>([]);
-  // const [isLoading, setIsLoading] = useState(true);
-  // const [totalPages, setTotalPages] = useState(0);
-  // const searchParams = useSearchParams();
+  const [people, setPeople] = useState<Person[]>([]);
+  const [isLoading, setIsLoading] = useState(true);
+  const [totalPages, setTotalPages] = useState(0);
+  const searchParams = useSearchParams();
 
-  // const loadPeople = useCallback(async () => {
-  //   setIsLoading(true);
+  const loadPeople = useCallback(async () => {
+    setIsLoading(true);
 
-  //   try {
-  //     const currentPage = Number(searchParams.get('page')) || 1;
-  //     const allPeople = await getAllHeroes(`?page=${currentPage}`);
+    try {
+      const currentPage = Number(searchParams.get('page')) || 1;
+      const allPeople = await getAllHeroes(`?page=${currentPage}`);
 
-  //     setPeople(allPeople.results);
-  //     setTotalPages(Math.ceil(allPeople.count / 10));
-  //   } catch (err) {
-  //     throw new Error(String(err));
-  //   } finally {
-  //     setIsLoading(false);
-  //   }
-  // }, [searchParams]);
+      setPeople(allPeople.results);
+      setTotalPages(Math.ceil(allPeople.count / 10));
+    } catch (err) {
+      throw new Error(String(err));
+    } finally {
+      setIsLoading(false);
+    }
+  }, [searchParams]);
 
-  // useEffect(() => {
-  //   void loadPeople();
-  // }, [loadPeople, searchParams]);
+  useEffect(() => {
+    void loadPeople();
+  }, [loadPeople, searchParams]);
 
   return (
     <div className="container flex flex-col items-center ml-auto mr-auto  p-5 gap-10">
