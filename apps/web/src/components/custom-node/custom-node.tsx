69548bab810d7a399213503fb159570abd74db01
mxmk <romamaksymuk@gmail.com>
2024-05-13 19:01:05 +0200

    props with data for child nodes & skeleton nodes with edges & all edges are implemented

--- apps/web/src/components/custom-node/custom-node.tsx
+++ apps/web/src/components/custom-node/custom-node.tsx
@@ -1,14 +1,19 @@
 import React, { memo } from 'react';
 import { Handle, Position } from 'reactflow';
 
-export const CustomNode = memo(function CustomNode(): JSX.Element {
+interface Props {
+  data: {
+    label: string
+  };
+}
+
+export const CustomNode = memo(function CustomNode({ data }: Props): JSX.Element {
   return (
     <div className="w-40 bg-neon-gradient rounded-xl p-0.5 relative">
       <div className="absolute inset-0 bg-neon-gradient rounded-xl blur-sm animate-pulse" />
 
-      <div className="relative size-full bg-black rounded-xl p-4 flex flex-col justify-between">
-        {/* {data} */}
-        dd
+      <div className="relative size-full bg-black rounded-xl p-4 flex flex-col justify-between text-center">
+        {data.label}
       </div>
       <Handle className="opacity-0" position={Position.Top} type="target" />
       <Handle className="opacity-0" position={Position.Bottom} type="source" />
