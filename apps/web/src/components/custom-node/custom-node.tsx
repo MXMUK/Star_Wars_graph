6b71921d49c3cf187ddcf4d4abfaa40f4191e3dd
mxmk <romamaksymuk@gmail.com>
2024-05-14 11:55:33 +0200

    added animation for nodes

--- apps/web/src/components/custom-node/custom-node.tsx
+++ apps/web/src/components/custom-node/custom-node.tsx
@@ -1,15 +1,19 @@
 import React, { memo } from 'react';
 import { Handle, Position } from 'reactflow';
+import { motion } from 'framer-motion';
 
 interface Props {
   data: {
-    label: string
+    label: string;
   };
 }
 
 export const CustomNode = memo(function CustomNode({ data }: Props): JSX.Element {
   return (
-    <div className="w-40 bg-neon-gradient rounded-xl p-0.5 relative">
+    <motion.div
+      animate={{ opacity: 1, scale: 1 }}
+      className="w-40 bg-neon-gradient rounded-xl p-0.5 relative"
+      initial={{ opacity: 0, scale: 0.7 }}>
       <div className="absolute inset-0 bg-neon-gradient rounded-xl blur-sm animate-pulse" />
 
       <div className="relative size-full bg-black rounded-xl p-4 flex flex-col justify-between text-center">
@@ -17,6 +21,6 @@ export const CustomNode = memo(function CustomNode({ data }: Props): JSX.Element
       </div>
       <Handle className="opacity-0" position={Position.Top} type="target" />
       <Handle className="opacity-0" position={Position.Bottom} type="source" />
-    </div>
+    </motion.div>
   );
 });
